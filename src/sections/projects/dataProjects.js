c30a7c1a90c60b203dcde0cedbe76936ba810fda
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-08-04 17:26:18 -0300

    feat: new project data.

---
+++ src/sections/projects/dataProjects.js
@@ -0,0 +1,14 @@
+const dataProjects = [
+  {
+    id: 1,
+    image: "assets/img/projeto-controle-estoque.png",
+    alt: "Image of the stock control project",
+    name: "Stock control",
+    stacks: ["React", "NodeJs", "MySQL"],
+    description:
+      "The project consists of a basic CRUD that allows the user to insert products into a stock. Thus, it is easy to know the products in large or smaller quantities, creating better control.",
+    link: "https://github.com/BaldezPatrick/controle-de-estoque",
+  },
+];
+
+export default dataProjects;
