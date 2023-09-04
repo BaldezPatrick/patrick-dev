66ce39f06011558512496a3129c5a1470f4bbcb1
Patrick Baldez <patrick.baldez@triforsys.com.br>
2023-09-04 20:07:56 -0300

    feat: new project on projects section and new link.

--- src/sections/projects/dataProjects.js
+++ src/sections/projects/dataProjects.js
@@ -7,7 +7,7 @@ const dataProjects = [
     stacks: ["React", "NodeJs", "MySQL"],
     description:
       "The project consists of a basic CRUD that allows the user to insert products into a stock. Thus, it is easy to know the products in large or smaller quantities, creating better control.",
-    link: "https://github.com/BaldezPatrick/controle-de-estoque",
+    linkGitHub: "https://github.com/BaldezPatrick/controle-de-estoque",
   },
   {
     id: 2,
@@ -17,7 +17,18 @@ const dataProjects = [
     stacks: ["Node", "Express", "MySQL", "Handlebars Template"],
     description:
       "MindSpace is a cutting-edge web application developed using the Node.js runtime environment, along with the robust Express framework. It follows the widely adopted MVC (Model-View-Controller) architectural pattern, providing a structured and efficient approach to building web applications.",
-    link: "https://github.com/BaldezPatrick/mindSpace",
+    linkGitHub: "https://github.com/BaldezPatrick/mindSpace",
+  },
+  {
+    id: 3,
+    image: "assets/img/timer-hero-project.png",
+    alt: "Image of the Timer Hero project",
+    name: "Timer Hero",
+    stacks: ["HTML", "CSS", "JavaScript"],
+    description:
+      "This project consists of a timer that can be started, paused, and restarted using interactive buttons. The time is displayed in a 24-hour format (hh:mm:ss) and is updated every second. Also, this is a project to show all the power of dynamism that JS offers to pages for manipulating the DOM..",
+    linkGitHub: "https://github.com/BaldezPatrick/timer-hero",
+    linkDemo: "https://timer-hero.vercel.app/",
   },
 ];
 
